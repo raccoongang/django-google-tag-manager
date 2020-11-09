bd7aeea88044a61d91f82a689aeb51c226953cc4
Sagirov Eugeniy <sagirov19@gmail.com>
2020-11-09 15:17:59 +0200

    refactor gtm_tag

--- gtm/templatetags/gtm_tags.py
+++ gtm/templatetags/gtm_tags.py
@@ -10,20 +10,19 @@ ri = register.inclusion_tag
 
 def gtm_tag(context, google_tag_id=None):
     request = context.get('request')
+    configuration_gtm_id = None
     if request:
         if request.COOKIES.get(
                 getattr(settings, 'GOOGLE_TAG_BYPASS_COOKIE', None)):
             return
 
-        if google_tag_id is None:
-            current_configuration = GTMConfiguration.get_current_config(request)
-            if current_configuration:
-                google_tag_id = current_configuration.gtm_id
+        current_configuration = GTMConfiguration.get_current_config(request)
+        if current_configuration:
+            configuration_gtm_id = current_configuration.gtm_id
 
-    if google_tag_id is None:
-        google_tag_id = getattr(settings, 'GOOGLE_TAG_ID', None)
+    default_gtm_id = getattr(settings, 'GOOGLE_TAG_ID', None)
 
-    context['google_tag_id'] = google_tag_id
+    context['google_tag_id'] = google_tag_id or configuration_gtm_id or default_gtm_id
 
     return context
 
